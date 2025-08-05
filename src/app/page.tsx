3b61fb490073b51fe31401e5b389574de9a49736
Shakiru <Olaiya.adeolu@gmail.com>
2025-08-05 19:54:21 +0100

    commiting sales dashboard for pushing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -67,12 +67,16 @@ export default function Home() {
 
       {/* main section */}
 
-      <main className="flex flex-row gap-3 my-8">
+      <main className="flex flex-row gap-3">
         {/* aside section */}
-        <aside className="w-[20%] bg-white mx-auto">
-          <img src="../images/01.jpg" className="w-[252px] h-[64px]" />
+        <aside className="w-[20%] bg-white mx-auto ">
+          {/* <img src="../images/01.jpg" className="w-[252px] h-[64px]" /> */}
+          <div className="bg-[#5d5fef] flex gap-2 py-3 mt-5 text-[white] text-[18px] w-[80%] h-[64px] rounded-[16px] mx-auto">
+            <img src="../images/graphy.png" alt=""  className="ml-4 w-[32px] h-[32px]" />
+            <h1 className="mt-2">Dashboard</h1>
+          </div>
 
-          <div className="text-center p-5 mx-auto flex justify-center">
+          <div className="text-center p-5  mx-auto flex justify-center">
             <a href="" className="flex gap-3 w-[172px] h-[32px] leading-7">
               <img src="../images/Group.jpg" className="w-3 h-3 mt-1 " alt="" />
               <h3
@@ -119,7 +123,7 @@ export default function Home() {
           <div className="text-center p-5 mx-auto flex justify-center">
             <a href="" className="flex gap-3 w-[172px] h-[32px] leading-7">
               <img
-                 src="../images/Chart_Line.jpg"
+                src="../images/Chart_Line.jpg"
                 className="w-3 h-3 mt-1 "
                 alt=""
               />
@@ -148,7 +152,7 @@ export default function Home() {
             </a>
           </div>
 
-           <div className="text-center p-5 mx-auto flex justify-center">
+          <div className="text-center p-5 mx-auto flex justify-center">
             <a href="" className="flex gap-3 w-[172px] h-[32px] leading-7">
               <img
                 src="../images/settings.jpg"
@@ -164,7 +168,7 @@ export default function Home() {
             </a>
           </div>
 
-           <div className="text-center p-5 mx-auto flex justify-center">
+          <div className="text-center p-5 mx-auto flex justify-center">
             <a href="" className="flex gap-3 w-[172px] h-[32px] leading-7">
               <img
                 src="../images/signOut.jpg"
@@ -179,89 +183,182 @@ export default function Home() {
               </h3>
             </a>
           </div>
-
         </aside>
 
         {/* display main content section */}
         <div className="w-[80%]">
-          <section className="px-3 flex flex-row gap-3 justify-between bg-yellow-500 w-[100%] mx-[auto] rounded-md">
+          <section className="px-3 py-3 flex flex-row gap-3 justify-between w-[100%] mx-[auto] rounded-md">
             {/* section one  starts */}
-            <div className="mt-5 w-[70%]">
+            <div className=" w-[70%]">
               <header className="mt-2">
-                <h2>Today's Sale</h2>
-                <h4>summary of sales</h4>
+                <h2
+                  className="text-xl leading-7 font-semibold "
+                  style={{
+                    color: "#05004E",
+                    fontWeight: "600",
+                    lineHeight: "32px",
+                  }}
+                >
+                  Today&apos;
+                  s Sale
+                </h2>
+                <h4
+                  className="text-[16px] leading-7 font-[400]"
+                  style={{
+                    color: "#737791",
+                    fontWeight: "400",
+                    lineHeight: "30px",
+                  }}
+                >
+                  summary of sales
+                </h4>
               </header>
-              <div className="flex flex-row gap-2">
-                <article className="bg-pink-200 p-2 rounded-md">
-                  <img src="../images/icon1.png" className="w-5 h-5" alt="" />
-                  <h1 className="mt-3 font-bold">$1k</h1>
-                  <h3 className="text-sm font-medium">Total Sales</h3>
-                  <p className="text-blue-900 font-semibold text-xs mt-2">
+              <div className="flex flex-row gap-3 ">
+                <article className="p-2 my-2 rounded-md w-[130px] h-[184px] rounded-[16px] bg-[#ffe2e5]">
+                  <img
+                    src="../images/icon1.png"
+                    className="w-[30px] h-[30px] mt-3"
+                    alt=""
+                  />
+                  <h1
+                    className="mt-3 text-xl font-semibold leading-8"
+                    style={{ color: "#151d48" }}
+                  >
+                    $1k
+                  </h1>
+                  <h4
+                    className="text-base font-medium leading-9"
+                    style={{ color: "#425166" }}
+                  >
+                    Total Sales
+                  </h4>
+                  <p
+                    className="text-[10px] font-medium leading-5"
+                    style={{ color: "#4079ed" }}
+                  >
                     +8% from yesterday
                   </p>
                 </article>
-                <article className="bg-pink-200 p-2 rounded-md">
-                  <img src="../images/icon1.png" className="w-5 h-5" alt="" />
-                  <h1 className="mt-3 font-bold">$1k</h1>
-                  <h3 className="text-sm font-medium">Total Sales</h3>
-                  <p className="text-blue-900 font-semibold text-xs mt-2">
-                    +8% from yesterday
+
+                <article className="p-2 my-2 rounded-md w-[130px] h-[184px] rounded-[16px] bg-[#fff4de]">
+                  <img
+                    src="../images/Icon (1).png"
+                    className="w-[30px] h-[30px] mt-3"
+                    alt=""
+                  />
+                  <h1
+                    className="mt-3 text-xl font-semibold leading-8"
+                    style={{ color: "#151d48" }}
+                  >
+                    300
+                  </h1>
+                  <h4
+                    className="text-base font-medium leading-9"
+                    style={{ color: "#425166" }}
+                  >
+                    Total Order
+                  </h4>
+                  <p
+                    className="text-[10px] font-medium leading-5"
+                    style={{ color: "#4079ed" }}
+                  >
+                    From 5% from yesterday
                   </p>
                 </article>
-                <article className="bg-pink-200 p-2 rounded-md">
-                  <img src="../images/icon1.png" className="w-5 h-5" alt="" />
-                  <h1 className="mt-3 font-bold">$1k</h1>
-                  <h3 className="text-sm font-medium">Total Sales</h3>
-                  <p className="text-blue-900 font-semibold text-xs mt-2">
-                    +8% from yesterday
+
+                <article className="p-2 my-2 rounded-md w-[130px] h-[184px] rounded-[16px] bg-[#dcfce7]">
+                  <img
+                    src="../images/pen.jpg"
+                    className="w-[30px] h-[30px] mt-3"
+                    alt=""
+                  />
+                  <h1
+                    className="mt-3 text-xl font-semibold leading-8"
+                    style={{ color: "#151d48" }}
+                  >
+                    5
+                  </h1>
+                  <h4
+                    className="text-base font-medium leading-9"
+                    style={{ color: "#425166" }}
+                  >
+                    Product Sold
+                  </h4>
+                  <p
+                    className="text-[10px] font-medium leading-5"
+                    style={{ color: "#4079ed" }}
+                  >
+                    From 1,2% from yesterday
                   </p>
                 </article>
-                <article className="bg-pink-200 p-2 rounded-md">
-                  <img src="../images/icon1.png" className="w-5 h-5" alt="" />
-                  <h1 className="mt-3 font-bold">$1k</h1>
-                  <h3 className="text-sm font-medium">Total Sales</h3>
-                  <p className="text-blue-900 font-semibold text-xs mt-2">
-                    +8% from yesterday
+
+                <article className="p-2 my-2 rounded-md w-[130px] h-[184px] rounded-[16px] bg-[#f3e8ff]">
+                  <img
+                    src="../images/plus.jpg"
+                    className="w-[30px] h-[30px] mt-2"
+                    alt=""
+                  />
+                  <h1
+                    className="mt-3 text-md font-semibold leading-8"
+                    style={{ color: "#151d48" }}
+                  >
+                    8
+                  </h1>
+                  <h4
+                    className="text-base font-medium leading-6"
+                    style={{ color: "#425166" }}
+                  >
+                    New Customers
+                  </h4>
+                  <p
+                    className="text-[10px] font-medium leading-5"
+                    style={{ color: "#4079ed" }}
+                  >
+                    0,5% from yesterday
                   </p>
                 </article>
               </div>
             </div>
 
             {/* section one ends */}
-            <div className="mt-4 bg-blue-300 h-[200px] w-[40%]">
-              <img src="../images/VI.png" className="h-[80%] w-[100%] mt-5" />
+            <div className="mt-15 h-[150px] w-[591px]">
+              <img src="../images/VI.png" className="h-[100%] w-[100%] mt-5" />
             </div>
           </section>
 
-          <section className="secSec flex flex-row">
+          <section className="secSec flex flex-row my-4">
             <img
               src="../images/revenue.png"
               alt=""
-              className="w-[30%] h-[30%]"
+              className="w-[40%] h-[351px]"
             />
-            <img src="../images/sati.png" alt="" className="w-[30%] h-[30%]" />
+            <img
+              src="../images/sati.png"
+              alt=""
+              className="w-[35%] h-[351px]"
+            />
             <img
               src="../images/target.png"
               alt=""
-              className="w-[30%] h-[30%]"
+              className="w-[25%] h-[351px]"
             />
           </section>
 
-          <section className="flex flex-row">
+          <section className="flex flex-row gap-3 w-[100%]">
             <img
               src="../images/TopProducts.jpg"
               alt=""
-              className="w-[30%] h-[30%]"
+              className="w-[40%] h-[351px]"
             />
             <img
               src="../images/salesMapping.jpg"
               alt=""
-              className="w-[30%] h-[30%]"
+              className="w-[30%] h-[351px]"
             />
             <img
               src="../images/Volume.jpg"
               alt=""
-              className="w-[30%] h-[30%]"
+              className="w-[25%] h-[351px]"
             />
           </section>
         </div>
